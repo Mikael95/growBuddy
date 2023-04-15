79950bbc1c8aaa7f75ea168295bc6cc77b69fe2d
Mikael95 <pk.mikael@outlook.com>
2023-04-15 21:25:13 +0200

    initial

---
+++ growBuddyWeb/Data/FieldUnit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class FieldUnit
+{
+	public FieldUnit()
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+        private List<Plant> plants;
+
+        public List<Plant> GetPlants()
+        {
+            return plants;
+        }
+
+        public void SetPlants(List<Plant> value)
+        {
+            plants = value;
+        }
+}
+}
---
+++ growBuddyWeb/Data/Plant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Plant
+{
+	public Plant()
+	{
+		public int Id {  get; set; }
+		public string Name { get; set; }
+		public float MinimumTemp { get; set; }
+		public float MaximumTemp { get; set; }
+		public float MinimumHumidity { get; set; }
+		public float MaximumHumidity { get;set; }
+		public float MinimumSunligtHours { get; set; }
+		public float MaximumSunligtHours { get;set; }
+		public float MaximumSunlightStrenght { get; set; }
+	}
+}
