d68fc583c55308d3cf46a7991be39126d59533a1
Mikael95 <pk.mikael@outlook.com>
2023-04-16 20:56:31 +0200

    added reading class

---
+++ growBuddyWeb/Data/Reading.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Reading
+{
+	public Reading()
+	{
+		public int Id {  get; set; }
+		public FieldUnit Unit { get; set; }
+		public Plant plant { get; set; }
+		public float Temp { get; set; }
+		public float Humidity { get; set; }
+		public float Sunlight { get; set; }
+		public DateTime Date { get; set; }
+	}
+}
