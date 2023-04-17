908bb0ce45cb5ba8c5d2777b3be276a2fa9faf34
Mikael95 <pk.mikael@outlook.com>
2023-04-17 17:09:56 +0200

    added DBContext

---
+++ growBuddyWeb/Data/PlantDataContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp1.Data
+{
+    public class PlantDataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public PlantDataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(Configuration.GetConnectionString("PlantsDB"));
+        }
+
+        public DbSet<Plant> Plants { get; set; }
+        public DbSet<FieldUnit> FieldUnits { get; set; }
+        public DbSet<Preferences> Preferences { get; set; }
+        public DbSet<Reading> Readings { get; set; }
+    }
+}
